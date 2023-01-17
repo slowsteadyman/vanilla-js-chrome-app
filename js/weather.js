15baf7db4a08f3691ded14ce5b037d75520d2569
slowsteadyman <117254015+slowsteadyman@users.noreply.github.com>
2023-01-17 18:47:58 +0900

    마무리
    weather api 코드 링크 제작

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,3 @@
-const city = document.querySelector("#weather span:nth-child(1)");
-const weather = document.querySelector("#weather span:nth-child(2)");
-const tempReal = document.querySelector("#weather span:nth-child(3)");
-const tempFeel = document.querySelector("#weather span:nth-child(4)");
-const info = document.querySelector("#weather span:nth-child(5)");
+const info = document.querySelector("#weather a");
 
-city.innerText = "도시 : ${citySaved}";
-weather.innerText = "날씨 : ${weatherSaved}";
-tempReal.innerText = "온도 : ${tempRealSaved}도";
-tempFeel.innerText = "체감 : ${tempFeelSaved}도";
 info.innerText = "실제로는 weather api 사용";
